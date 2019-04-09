7e6df4e7ee217a402a8396ecd724fe42c55a08d2
toricheli <toricheli@syneforge.com>
2019-04-09 10:08:11 +0300

    commit need refactor

--- web/js/dashboard_bkng.js
+++
@@ -1,7 +0,0 @@
-jQuery( document ).ready(function() {
-    jQuery(".dashboard-form").on("submit", function (event) {
-        if (!confirm(bkng_messages.message_confirm_before_delete_booking)) {
-            event.preventDefault();
-        }
-    });
-});
